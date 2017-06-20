ddc561d2a73626d62f1982f8ef170081999c3c11
benjaminoh1 <benjamin_oh@naver.com>
2017-06-20 16:11:15 +0900

    study

--- Chapter 01/matrices.py
+++ Chapter 01/matrices.py
@@ -10,49 +10,28 @@ ops.reset_default_graph()
 sess = tf.Session()
 
 # 2차원 매트릭스는 Numpy arrays를 통하여 만들 수 있다. 혹은 앞에서 말한 데로 tf.zeros(), tf.ones(), tf.truncated_normal() 등으로 만들 수 있다.
+identity_matrix = tf.diag([1.0,1.0,1.0])     # 이런 식으로 대각행렬을 만들 수 있다.
+A = tf.truncated_normal([2,3])              # 
+B = tf.fill([2,3], 5.0)                             # 5로 가득찬 2x3 행렬을 만드는 방법.
+C = tf.random_uniform([3,2])                # 0~1범위 내 랜덤한 숫자로 가득 찬 3x2행렬을 만드는 방법
+D = tf.convert_to_tensor(np.array([[1., 2., 3.], [-3., -7., -1.], [0., 5., -2.]]))  # Np.array로부터 행렬을 만드는 방법.
 
-# Identity matrix
-identity_matrix = tf.diag([1.0,1.0,1.0])
 print(sess.run(identity_matrix))
-
-# Diagonal matrix
-diag()
-
-# 2x3 random norm matrix
-A = tf.truncated_normal([2,3])
 print(sess.run(A))
-
-# 2x3 constant matrix
-B = tf.fill([2,3], 5.0)
 print(sess.run(B))
-
-# 3x2 random uniform matrix
-C = tf.random_uniform([3,2])
-print(sess.run(C))
-print(sess.run(C)) # Note that we are reinitializing, hence the new random variabels
-
-# Create matrix from np array
-D = tf.convert_to_tensor(np.array([[1., 2., 3.], [-3., -7., -1.], [0., 5., -2.]]))
+print(sess.run(C))                              #이런 경우에 sess.run(C)을 reinitialize한다면, 다른 랜덤한 숫자로 가득 찬다는 것을 기억해두자. 
 print(sess.run(D))
 
-# Matrix addition/subtraction
-print(sess.run(A+B))
-print(sess.run(B-B))
 
-# Matrix Multiplication
-print(sess.run(tf.matmul(B, identity_matrix)))
+# 행렬의 연산
+print(sess.run(A+B))                                   #행렬합
+print(sess.run(B-B))                                    #행렬차
+print(sess.run(tf.matmul(B, identity_matrix)))    #행렬곱
 
-# Matrix Transpose
-print(sess.run(tf.transpose(C))) # Again, new random variables
+#행렬함수들
+print(sess.run(tf.transpose(C)))                       #  대각행렬 만들기. 앞에서 말한 데로 C를 reinitialize하였으므로, 앞과 다른 랜덤한 숫자로 가득 찬다.
+print(sess.run(tf.matrix_determinant(D)))           # Determinant
+print(sess.run(tf.matrix_inverse(D)))                   # Matrix Inverse
+print(sess.run(tf.cholesky(identity_matrix)))           # Cholesky Decomposition
+print(sess.run(tf.self_adjoint_eig(D)))                     # Eigenvalues and Eigenvectors. 연산하고 나면 1행들에는 eigenvalues가, 나머지 행들에는 해당 eigenvector가 출력된다.
 
-# Matrix Determinant
-print(sess.run(tf.matrix_determinant(D)))
-
-# Matrix Inverse
-print(sess.run(tf.matrix_inverse(D)))
-
-# Cholesky Decomposition
-print(sess.run(tf.cholesky(identity_matrix)))
-
-# Eigenvalues and Eigenvectors
-print(sess.run(tf.self_adjoint_eig(D)))
--- Chapter 01/operations.py
+++ Chapter 01/operations.py
@@ -1,40 +1,53 @@
 # Operations
-#----------------------------------
-#
-# This function introduces various operations
-# in Tensorflow
+# This function introduces various operations in Tensorflow
+# Tensorflow graph에 사용할 수 있는 iperation에 대해 알아보자.
 
-# Declaring Operations
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework import ops
 ops.reset_default_graph()
 
-# Open graph session
-sess = tf.Session()
 
-# div() vs truediv() vs floordiv()
-print(sess.run(tf.div(3,4)))
-print(sess.run(tf.truediv(3,4)))
-print(sess.run(tf.floordiv(3.0,4.0)))
+sess = tf.Session()         # Open graph session
+
+# div()
+# - tensorflow에서는 div()와 그 변형함수들을 제공한다.
+print(sess.run( tf.div(3,4)))                # - div()는 입력과 동일한 type을 출력한다. 예를들어 입력이 int였다면 출력도 int이다. 따라서 이 계산의 출력은 0이다.
+print(sess.run( tf.truediv(3,4)))           # - truediv()는 입력과 상관없이 항상 floats를 출력한다. 따라서 출려은 0.75이다.
+print(sess.run( tf.floordiv(3.0,4.0)))       # - floordiv()는 float입력에서 int출력(정확히는 계산값보다 작으면서 가장 가까운 int를 float으로)을 내놓는다. 출력은 0.0이다.
 
 # Mod function
-print(sess.run(tf.mod(22.0,5.0)))
+print(sess.run( tf.mod(22.0,5.0)))               # - mod()
 
-# Cross Product
-print(sess.run(tf.cross([1.,0.,0.],[0.,1.,0.])))
+# 기타 연산/함수들
+print(sess.run( tf.cross([1.,0.,0.],[0.,1.,0.])))                       #  Cross Product. - cross()는 2개의 3차원 벡터에서만 정의되므로, 이 때만 사용 가능하다. 출력값은 [0 0 1]
 
-# Trig functions
-print(sess.run(tf.sin(3.1416)))
-print(sess.run(tf.cos(3.1416)))
-# Tangemt
-print(sess.run(tf.div(tf.sin(3.1416/4.), tf.cos(3.1416/4.))))
+print(sess.run( tf.sin(3.1416)))                                     # - 입력 tensor에 대한 삼각함수.
+print(sess.run( tf.cos(3.1416)))                                     # - 입력 tensor에 대한 삼각함수.
+print(sess.run( tf.div(tf.sin(3.1416/4.), tf.cos(3.1416/4.))))    # - 입력 tensor에 대한 삼각함수.
+
+ # print(sess.run( tf.abs()                                                # - 절대값 
+ # print(sess.run( tf.celi                                                  # - celiing function(바닥함수)
+ # print(sess.run( tf.exp
+ # print(sess.run( tf.floor
+ # print(sess.run( tf.int
+ # print(sess.run( tf.log
+ # print(sess.run( tf.maximum                                           # - 원소별로 비교하여, x와 y중 가장 큰 함수를 출력으로 내보냄 
+ # print(sess.run( tf.minimum
+ # print(sess.run( tf.neg
+ # print(sess.run( tf.pow
+ # print(sess.run( tf.round
+ # print(sess.run( tf.rsqrt
+ # print(sess.run( tf.sign
+ # print(sess.run( tf.sqrt
+ # print(sess.run( tf.square
+
 
 # Custom operation
 test_nums = range(15)
-#from tensorflow.python.ops import math_ops
-#print(sess.run(tf.equal(test_num, 3)))
+# from tensorflow.python.ops import math_ops
+# print(sess.run(tf.equal(test_num, 3)))
 def custom_polynomial(x_val):
     # Return 3x^2 - x + 10
     return(tf.sub(3 * tf.square(x_val), x_val) + 10)
