f913fee1e371bda78b0a7ef8bb6a302952ee52f4
benjaminoh1 <benjamin_oh@naver.com>
2017-06-21 17:24:51 +0900

    study

--- Chapter 02/layering_nested_operations.py
+++ Chapter 02/layering_nested_operations.py
@@ -1,35 +1,34 @@
 # Layering Nested Operations
+# - 데이터가 형태가 바뀌는 것도 중요하다. 예를 들어 3x5행렬에 5x1행렬을 곱한다음, 다시 1x1행렬을 곱한다면 최종적으로는 3x1행렬이 된다.
 
+# 기본 세팅
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-# Create graph
+# Computational Graph 생성
 sess = tf.Session()
 
-# Create tensors
-
-# Create data to feed in
+#Feed 할 데이터의 생성
 my_array = np.array([[1., 3., 5., 7., 9.],
-                   [-2., 0., 2., 4., 6.],
-                   [-6., -3., 0., 3., 6.]])
+                                 [-2., 0., 2., 4., 6.],
+                                 [-6., -3., 0., 3., 6.]])
+
 x_vals = np.array([my_array, my_array + 1])
-x_data = tf.placeholder(tf.float32, shape=(3, 5))
+x_data = tf.placeholder(tf.float32, shape=(3, 5))       #혹은 필요하다면, 데이터 크기를 모를 경우 tf.placeholder(tf.float32, shape=(3,none)) 으로 해도 될 것이다.
+
 m1 = tf.constant([[1.],[0.],[-1.],[2.],[4.]])
 m2 = tf.constant([[2.]])
 a1 = tf.constant([[10.]])
 
-# 1st Operation Layer = Multiplication
-prod1 = tf.matmul(x_data, m1)
+# 이제 operation을 해 보자 
+prod1 = tf.matmul(x_data, m1)           # 1st Operation Layer = Multiplication
+prod2 = tf.matmul(prod1, m2)           # 2nd Operation Layer = Multiplication
+add1 = tf.add(prod2, a1)                    # 3rd Operation Layer = Addition
 
-# 2nd Operation Layer = Multiplication
-prod2 = tf.matmul(prod1, m2)
-
-# 3rd Operation Layer = Addition
-add1 = tf.add(prod2, a1)
-
+#마지막으로 데이터를 Feed하면 된다. 
 for x_val in x_vals:
     print(sess.run(add1, feed_dict={x_data: x_val}))
 
-merged = tf.merge_all_summaries()
-my_writer = tf.train.SummaryWriter('/home/nick/OneDrive/Documents/tensor_flow_book/Code/2_Tensorflow_Way', sess.graph)
+merged = tf.summary.merge_all()
+my_writer = tf.train.SummaryWriter('C:\Users\user\Documents\GitHub\tensorflowcookbook\Chapter 02', sess.graph)
--- Chapter 02/multiple_layers.py
+++ Chapter 02/multiple_layers.py
@@ -1,48 +1,54 @@
-# Layering Nested Operations
+# Layering Nested Operations & graph visualization
+# - 이제 각 layer들마다 어떻게 data를 이동시킬 지 연습해 보도록 하겠다.
+# - 입력으로 작은 사이즈의 2D 이미지가 들어온다고 하고 시작해보자.
+
+# 기본 세팅 - tf와 Numpy를 불러온다.
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework import ops
 ops.reset_default_graph()
 
-# Create graph
+# Computational Graph 생성
 sess = tf.Session()
 
-# Create tensors
 
-# Create a small random 'image' of size 4x4
+# 일단 입력으로 쓰일 2D 4x4이미지를 만든다. [1,4,4,1]=[이미지번호, 높이, 폭, 채널]을 이야기한다. Tensorflow에서는 이런 식으로 이미지를 표현한다.
 x_shape = [1, 4, 4, 1]
 x_val = np.random.uniform(size=x_shape)
 
+
+#이 data를 받을 placeholder를 설정한다.
 x_data = tf.placeholder(tf.float32, shape=x_shape)
 
-# Create a layer that takes a spatial moving window average
-# Our window will be 2x2 with a stride of 2 for height and width
-# The filter value will be 0.25 because we want the average of the 2x2 window
-my_filter = tf.constant(0.25, shape=[2, 2, 1, 1])
-my_strides = [1, 2, 2, 1]
-mov_avg_layer= tf.nn.conv2d(x_data, my_filter, my_strides,
-                            padding='SAME', name='Moving_Avg_Window')
+
+# 입력 4x4 이미지를 2칸 stride로 움직이며 평균을 내는 2x2 window를 만들기 위하여, conv2d()를 사용하겠다. 
+# 더불어 4개의 2x2이미지의 평균을 내기 위하여, 상수 0.25를 곱하겠다. 
+my_filter = tf.constant(0.25, shape=[2, 2, 1, 1])                           # 상수를 만드는 부분. 0.25로 가득 찬, [filter_height, filter_width, in_channels, out_channels]을 만들겠다는 거다.
+my_strides = [1, 2, 2, 1]                                                               # strides는 2칸 2칸씩 움직이겠다는 이야기. Strides는 [1, stride, stride, 1]로 항상 만들어야 한다.
+mov_avg_layer= tf.nn.conv2d(x_data, my_filter, my_strides, padding='SAME', name='Moving_Avg_Window')
+
 
 # Define a custom layer which will be sigmoid(Ax+b) where
 # x is a 2x2 matrix and A and b are 2x2 matrices
 def custom_layer(input_matrix):
-    input_matrix_sqeezed = tf.squeeze(input_matrix)
+    input_matrix_sqeezed = tf.squeeze(input_matrix)                 # shape(squeeze(t))는 size가 1인 dimension을 제거한다. 예를 들어 [1,4,4,1]->[4,4]로 만든다. 여기에선 이미지의 픽셀부분만 남기겠다는 거다.
     A = tf.constant([[1., 2.], [-1., 3.]])
     b = tf.constant(1., shape=[2, 2])
     temp1 = tf.matmul(A, input_matrix_sqeezed)
     temp = tf.add(temp1, b) # Ax + b
-    return(tf.sigmoid(temp))
+    return(tf.sigmoid(temp))                                                        # 마지막으로 sigmoid function을 적용하였다.
 
-# Add custom layer to graph
+
+# Comp. graph에 위에 만든 layer를 넣어보자.
 with tf.name_scope('Custom_Layer') as scope:
     custom_layer1 = custom_layer(mov_avg_layer)
 
-# The output should be an array that is 2x2, but size (1,2,2,1)
-#print(sess.run(mov_avg_layer, feed_dict={x_data: x_val}))
 
-# After custom operation, size is now 2x2 (squeezed out size 1 dims)
+# 마지막으로 session을 시행하면 된다.
 print(sess.run(custom_layer1, feed_dict={x_data: x_val}))
 
-merged = tf.merge_all_summaries()
-my_writer = tf.train.SummaryWriter('/home/nick/OneDrive/Documents/tensor_flow_book/Code/2_Tensorflow_Way', sess.graph)
+
+# Graph를 눈으로 봐 보자.
+file_writer = tf.summary.FileWriter('C:/Users/user/Documents/GitHub/tensorflowcookbook', sess.graph)                # sess.graph contains the graph definition; that enables the Graph Visualizer.
+tensorboard --logdir=training:C:/Users/user/Documents/GitHub/tensorflowcookbook                                           # 이 부분만 cmd에서 따로 시행하여야만이 된다. Tensorflow는 :와 --를 구분 못하기 때문 
